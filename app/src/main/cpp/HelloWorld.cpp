236dac825c63ea1d21daf76d0b2341c3ec2eb0cd
MichGur <michael@thegur.net>
2020-08-13 17:55:51 +0300

    linked c++ assimp

--- app/src/main/cpp/HelloWorld.cpp
+++ app/src/main/cpp/HelloWorld.cpp
@@ -4,8 +4,20 @@
 
 #include <jni.h>
 #include <string>
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
 
-extern "C" JNIEXPORT jstring JNICALL
-Java_com_klmn_misery_MiseryJNI_helloWorld(JNIEnv *env, jobject thiz) {
-    return env->NewStringUTF("Hello from JNI!");
+extern "C" JNIEXPORT jstring
+Java_com_klmn_misery_MiseryJNI_helloWorld(JNIEnv *env, jobject ths) {
+//    Assimp::Importer importer;
+//    const aiScene* scene = importer.ReadFile( "./pig.obj",
+//                                              aiProcess_CalcTangentSpace       |
+//                                              aiProcess_Triangulate            |
+//                                              aiProcess_JoinIdenticalVertices  |
+//                                              aiProcess_SortByPType);
+//
+//    if( !scene) return env->NewStringUTF("failed");
+
+    return env->NewStringUTF("success!");
 }
