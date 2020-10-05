db27b0971fea288d0f199905cbda6f95cfcd9d50
MichGur <michael@thegur.net>
2020-10-05 16:18:09 +0300

    removed old view class

--- app/src/main/java/com/klmn/misery/Misery.kt
+++ app/src/main/java/com/klmn/misery/Misery.kt
@@ -5,27 +5,20 @@ import android.os.Bundle
 import com.klmn.misery.examples.FlappyPig
 import com.klmn.misery.jni.MiseryJNI
 import com.klmn.misery.render.MiserySurfaceView
-import com.klmn.misery.render.MiseryView
 
 /**
  * ಠ^ಠ.
  * Created by Michael on 12/14/2018.
  */
-const val NATIVE_SURFACE = true
 
 class Misery : Activity()
 {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        if (!NATIVE_SURFACE) {
-            val view = MiseryView(FlappyPig(this))
-            setContentView(view)
-        } else {
-            val view = MiserySurfaceView(this)
-            setContentView(view)
-            FlappyPig(this).init()
-        }
+        val view = MiserySurfaceView(this)
+        setContentView(view)
+        FlappyPig(this).init()
     }
 
     override fun onDestroy() {
--- app/src/main/java/com/klmn/misery/render/MiserySurfaceView.kt
+++ app/src/main/java/com/klmn/misery/render/MiserySurfaceView.kt
@@ -5,6 +5,18 @@ import android.view.SurfaceHolder
 import android.view.SurfaceView
 import com.klmn.misery.jni.MiseryJNI
 
+/* TODO:
+    -separate MiseryView from GLThread, render gl content to a buffer, render android ui on top
+    -perhaps move ALL code except for JNI to native
+    -figure out ui & input management, should be separate from the ecs and as close as possible to
+    regular android app ui
+    -native code- urgent organization, try avoiding global variables (have create and set methods
+    to avoid increased JNI traffic)
+    -debug mesh loading code- some serious bugginess there
+    -give Game class some actual options
+    -a proper render & physics engines
+    -turn this into a library and export
+ */
 
 class MiserySurfaceView(context: Context) : SurfaceView(context), SurfaceHolder.Callback {
     init { holder.addCallback(this) }
